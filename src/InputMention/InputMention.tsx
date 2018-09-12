2a4894f2e0de4619f11eeca0a9217eb911ccc4bc
Zsolt Turi <turi.zsolt.m@gmail.com>
2018-09-13 00:52:44 +0200

    refactoring

--- src/InputMention/InputMention.tsx
+++ src/InputMention/InputMention.tsx
@@ -82,73 +82,83 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
         );
     }
 
-    private onKeyDown(event: any) {
-        let showOptions = this.state.showOptions;
-        
-        if(showOptions && event.which === 13) {
-            console.log("enter");
-
-            let insertText = this.state.text.substring(this.state.startsFrom, event.target.selectionEnd);
-            const size = this.props.list.filter(this.filterCondition).length;
-            if(size > 0) {
-                insertText = this.props.list[this.state.currentOptionIndex].mentionTag;
-            }
-            this.setState({
-                text: 
-                    this.state.text.substring(0, this.state.startsFrom) +
-                    insertText +
-                    this.state.text.substr(event.target.selectionEnd) +
-                    " "
-            });
-            this.setState({
-                showOptions: false
-            });
-            showOptions = false;
-
-            event.preventDefault();
-        }
-
-        if(showOptions && event.which === 27) {
-            console.log("esc");
-
-            this.setState({
-                showOptions: false
-            });
-            showOptions = false;
-
-            event.preventDefault();
-
-        }
-
-        if(showOptions && event.which === 38) {
-            console.log("up");
-
-            this.setState((state) => {
-                return {
-                    currentOptionIndex: this.prev(state.currentOptionIndex)
-                };
-            });
-
-            event.preventDefault();
-
-        }
-
-        if(showOptions && event.which === 40) {
-            console.log("down");
-
-            this.setState((state) => {
-                return {
-                    currentOptionIndex: this.next(state.currentOptionIndex)
-                };
-            });
-
-            event.preventDefault();
-
-
-        }
-
-        
+    private onTextChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({
+            text: event.target.value
+        });
     }
+
+    private onKeyDown(event: any) {
+        if (!this.state.showOptions) { return; }
+        
+        if (this.isEnter(event)) {
+            this.handleEnter(event);
+        } else if (this.isEscape(event)) {
+            this.handleEscape(event);
+        } else if (this.isUp(event)) {
+            this.handleUp(event);
+        } else if (this.isDown(event)) {
+            this.handleDown(event);
+        }
+    }
+
+    private isDown(event: any) {
+        return event.which === 40;
+    }
+
+    private isUp(event: any) {
+        return event.which === 38;
+    }
+
+    private isEscape(event: any) {
+        return event.which === 27;
+    }
+
+    private isEnter(event: any) {
+        return event.which === 13;
+    }
+
+    private handleDown(event: any) {
+        this.setState((state) => ({ currentOptionIndex: this.next(state.currentOptionIndex) }));
+        event.preventDefault();
+    }
+
+    private handleUp(event: any) {
+        this.setState((state) => ({ currentOptionIndex: this.prev(state.currentOptionIndex) }));
+        event.preventDefault();
+    }
+
+    private handleEscape(event: any) {
+         this.setState({ showOptions: false });
+        event.preventDefault();
+    }
+
+    private handleEnter(event: any) {
+        this.setState({ 
+            showOptions: false,
+            text: this.insertMentionToText(event)
+        });
+        event.preventDefault();
+    }
+
+    private insertMentionToText(event: any) {
+        return this.state.text.substring(0, this.state.startsFrom) +
+            this.getInsertableMention(event) +
+            this.state.text.substr(event.target.selectionEnd) +
+            " ";
+    }
+
+    private getInsertableMention(event: any) {
+        return this.isFilteredListHasElements() ?
+            this.props.list[this.state.currentOptionIndex].mentionTag :
+            this.state.text.substring(this.state.startsFrom, event.target.selectionEnd);
+    }
+
+    private isFilteredListHasElements() {
+        return this.props.list.filter(this.filterCondition).length > 0;
+    }
+
+    /************/
 
     private filterCondition(item: InputMentionListItem): boolean {
         return this.filterConditionText(item, this.state.searchText);
@@ -200,7 +210,6 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
         let showOptions = this.state.showOptions;
         
         if(charAtSelection === "@" && charBeforeSelection === " ") {
-            console.log("at", event.target.selectionStart-1);
             this.setState({
                 showOptions: true,
                 startsFrom: event.target.selectionStart
@@ -220,15 +229,11 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
                     showOptions: false
                 });
                 showOptions = false;
-            } else {
-                console.log("search", this.state.text.substring(this.state.startsFrom, event.target.selectionEnd+1));
             }
         }
 
-        console.log("current", showOptions, this.state.currentOptionIndex);
         if(showOptions && !this.filterConditionText(this.props.list[this.state.currentOptionIndex], searchText)) {
-            console.log("correct it to", this.nextText(-1, searchText));
-
+            
             this.setState((state) => {
                 return {
                     currentOptionIndex: this.nextText(-1, searchText)
@@ -237,9 +242,5 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
         }
     }
 
-    private onTextChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        this.setState({
-            text: event.target.value
-        });
-    }
+    
 }
