3e9d50b3dcdb1c6f0f250700c5873535a93a40d9
Zsolt Turi <turi.zsolt.m@gmail.com>
2018-09-12 22:53:04 +0200

    Default operations.

--- src/InputMention/InputMention.tsx
+++ src/InputMention/InputMention.tsx
@@ -15,7 +15,10 @@ export interface InputMentionState {
         left: number,
         top: number
     };
+    searchText: string;
     showOptions: boolean;
+    startsFrom: number;
+    text: string;
 }
 
 export class InputMention extends React.Component<InputMentionProps, InputMentionState> {
@@ -27,14 +30,24 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
                 left: 100,
                 top: 100
             },
-            showOptions: true
+            searchText: "",
+            showOptions: false, // true,
+            startsFrom: 0,
+            text: ""
         };
+
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
     }
 
     public render() {
         return (
             <div style={{position: "relative"}}>
-                <textarea />
+                <textarea 
+                    onKeyUp={this.onKeyDown}
+                    onChange={this.onTextChange}
+                    value={this.state.text}
+                />
                 <div style={{
                     display: this.state.showOptions ? "block" : "none",
                     left: this.state.optionPosition.left + "px",
@@ -50,5 +63,54 @@ export class InputMention extends React.Component<InputMentionProps, InputMentio
                 </div>
         </div>
         );
+    }
+
+    private onKeyDown(event: any) {
+        const myChar = this.state.text.substr(event.target.selectionStart-1,1);
+        let showOptions = this.state.showOptions;
+        
+        if(myChar === "@") {
+            console.log("at", event.target.selectionStart-1);
+            this.setState({
+                showOptions: true,
+                startsFrom: event.target.selectionStart
+            });
+            showOptions = true;
+        }
+
+        if(showOptions) {
+            this.setState({
+                searchText: this.state.text.substring(this.state.startsFrom, event.target.selectionEnd+1)
+            });
+        }
+        console.log("search", this.state.text.substring(this.state.startsFrom, event.target.selectionEnd+1));
+
+        if(event.which === 13) {
+            console.log("enter");
+        }
+
+        if(event.which === 27) {
+            console.log("esc");
+
+            this.setState({
+                showOptions: false
+            });
+        }
+
+        if(event.which === 38) {
+            console.log("up");
+        }
+
+        if(event.which === 40) {
+            console.log("down");
+        }
+
+        console.log("keyDown", event, event.target.selectionStart, event.target.selectionEnd, myChar, event.which, this.state.text);
+    }
+
+    private onTextChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        this.setState({
+            text: event.target.value
+        });
     }
 }
