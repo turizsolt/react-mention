f82930180b2cdfa24047e8a96e214bce3940f85d
Zsolt Turi <turi.zsolt.m@gmail.com>
2018-09-13 14:55:51 +0200

    characters updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import './App.css';
 import { InputMention, InputMentionListItem } from './InputMention/InputMention';
 
 const optionList:InputMentionListItem[] = [
-  {mentionTag: "hucky", text: "Huckleberry Finn", imageUrl: "https://freekidsbooks.org/wp-content/uploads/2017/01/huckleberry-finn-updated-language-version-1.png"},
-  {mentionTag: "matyi", text: "Mátyás, az Igazságos", imageUrl: "http://www.ivad.hu/2016/wp-content/uploads/2016/03/image.aspx_.jpg"},
-  {mentionTag: "mz/x", text: "Köbüki", imageUrl: "https://www.mindenegyben.com/post_image/2017/08/1000_19510337_1763032007057762_2447845037042409697_n_20170823_151832.jpg"},
-  {mentionTag: "aladar", text: "Mézga Aladár", imageUrl: "https://i.ytimg.com/vi/vBLC0cOyGuU/hqdefault.jpg"},
-  {mentionTag: "mgeza", text: "Mézga Géza", imageUrl: "https://m.blog.hu/mu/musorvizio/amezgacsaladfolytatas.jpg"},
-  {mentionTag: "gerevicha", text: "Gerevich Aladár", imageUrl: "https://upload.wikimedia.org/wikipedia/ru/thumb/c/c3/%D0%90%D0%BB%D0%B0%D0%B4%D0%B0%D1%80_%D0%93%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%D1%87.jpg/236px-%D0%90%D0%BB%D0%B0%D0%B4%D0%B0%D1%80_%D0%93%D0%B5%D1%80%D0%B5%D0%B2%D0%B8%D1%87.jpg"},
+  {mentionTag: "hucky", text: "Huckleberry Finn", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/56/Huckleberry-finn-with-rabbit.jpg"},
+  {mentionTag: "matyas", text: "Mátyás, az Igazságos", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Matthias_Corvinus.jpg"},
+  {mentionTag: "mzperx", text: "Köbüki", imageUrl: "https://upload.wikimedia.org/wikipedia/hu/7/7b/Mzper_X.png"},
+  {mentionTag: "aladar", text: "Mézga Aladár", imageUrl: "https://upload.wikimedia.org/wikipedia/hu/4/40/M%C3%A9zga_Alad%C3%A1r_k%C3%BCl%C3%B6n%C3%B6s_kalandjai.jpg"},
+  {mentionTag: "mgeza", text: "Mézga Géza", imageUrl: "https://upload.wikimedia.org/wikipedia/hu/f/f6/M%C3%A9zga_csal%C3%A1d.png"},
+  {mentionTag: "foxi", text: "Foxi Maxi", imageUrl: "https://upload.wikimedia.org/wikipedia/hu/thumb/8/81/Foxi_maxi.jpg/250px-Foxi_maxi.jpg"},
 ];
 
 class App extends React.Component {
